4e3df3209d110523f1df9921bd0046c54399393c
CrazyStudios <techshanvir32@gmail.com>
2024-02-05 18:35:21 +0530

    activity.php

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
-    'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'account-activity-log' => 'ਖਾਤਾ ਸਰਗਰਮੀ ਲਾਗ',
+    'activity-log' => 'ਸਰਗਰਮੀ ਲਾਗ',
+    'clear-filters' => 'ਫਿਲਟਰ ਸਾਫ ਕਰੋ',
+    'metadata' => 'ਮੈਟਾਡਾਟਾ',
+    'close' => 'ਬੰਦ ਕਰੋ',
+    'no-logs-server' => 'ਇਸ ਸਰਵਰ ਲਈ ਕੋਈ ਸਰਗਰਮੀ ਲਾਗ ਉਪਲਬਧ ਨਹੀਂ ਹੈ।'
 ];
